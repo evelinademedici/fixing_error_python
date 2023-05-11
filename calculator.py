97e5518454972d8ba6ad53858ceb9ef6c5598202
evelinademedici <131269973+evelinademedici@users.noreply.github.com>
2023-05-11 16:23:35 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def add(a, b):
-    return a + b
+    return a ++ b
 
 def subtract(a, b):
     return a - b
@@ -7,7 +7,7 @@ def subtract(a, b):
 number_one = int(input("Enter a number: ")) #added int()
 number_two = int(input("Enter another number: "))
 
-message = "The result of " + str(number_one) + " + " + str(number_two) + " is " + str(add(number_one, number_two)) # added str()
+message = "The result of " + str(`number_one) + " + " + str(number_two) + " is " + str(add(number_one, number_two)) # added str()
 print(message)
 
 message = "The result of " + str(number_one) + " - " + str(number_two) + " is " + str(subtract(number_one, number_two))   
